7f741f9fd880a29c1b94ff4c3f68ee0dec97da8f
AlexBatura1988 <alex.batura88@gmail.com>
2022-05-07 18:35:57 +0300

    finish ex

--- WorkingWithFiles/src/exAlice/Runner.java
+++ WorkingWithFiles/src/exAlice/Runner.java
@@ -2,33 +2,66 @@ package exAlice;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Runner {
 
 	public static void main(String[] args) {
 		alice();
-		
 
 	}
+
 	public static void alice() {
-		
+
 		int wordCount = 0;
-		int lineCount = 0;
+		int lineCount = 1;
 		String word = "";
 		String sentence = "";
-		
-		try(FileReader reader = new FileReader("myFiles/alice.txt")){
-			
+
+		try (FileReader reader = new FileReader("myFiles/alice.txt")) {
+			int text;
+			do {
+				text = reader.read();
+				char ch = (char) text;
+				if (ch == '\n') {
+					lineCount++;
+					wordCount++;
+				} else if (ch == ' ') {
+					wordCount++;
+				}
+				if (lineCount == 18) {
+					sentence += ch;
+				}
+				if (wordCount == 578) {
+					word += ch;
+				}
+
+			} while (text != -1);
+
 		} catch (FileNotFoundException e) {
-			
+
 			e.printStackTrace();
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+		System.out.println("line count is: " + lineCount);
+		System.out.println("world count is: " + wordCount);
+		System.out.println("sentence 18 is: " + sentence);
+		System.out.println("word 578 is: " + word);
+
+		try (FileWriter writer = new FileWriter("myFiles/output.txt")) {
+			writer.write("word 578 is: " + word +"\n");
+			writer.write("line count is: " + lineCount + "\n");
+			writer.write("world count is: " + wordCount + "\n");
+			writer.write("sentence 18 is: " + sentence);
+			
+
 		} catch (IOException e) {
 			
 			e.printStackTrace();
 		}
-		
-		
 	}
 
 }
