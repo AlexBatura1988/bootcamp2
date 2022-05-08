a0055959dc81496799eaf39e1f2139fbc83148a2
AlexBatura1988 <alex.batura88@gmail.com>
2022-05-08 13:03:52 +0300

    generic run

--- Generic/src/ex1/Runner.java
+++ Generic/src/ex1/Runner.java
@@ -7,19 +7,11 @@ import java.util.List;
 public class Runner {
 
 	public static void main(String[] args) {
-		
+
 		MyList<Integer> t = new MyList<>(5);
-		List<Integer> list = Arrays.asList(100,200,300);
-		//List<Integer> rev = t.reverse(list);
+		List<Integer> list = Arrays.asList(100, 200, 300);
 		t.reverse(list);
-		
 		System.out.println(list);
-		
-		
-		
-		
-		
-	
 
 	}
 
