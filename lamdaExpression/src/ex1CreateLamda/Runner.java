e694a03c82453bf8e863eaef07120b557c12165b
AlexBatura1988 <alex.batura88@gmail.com>
2022-05-09 20:53:56 +0300

    finished ex1

--- lamdaExpression/src/ex1CreateLamda/Runner.java
+++ lamdaExpression/src/ex1CreateLamda/Runner.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Runner {
 
@@ -12,44 +13,34 @@ public class Runner {
 		Person person1 = new Person("Den", "Scoth", 30);
 		Person person2 = new Person("Bob", "Martin", 60);
 		Person person3 = new Person("Alice", "Smith", 39);
-		Person person4 = new Person("Anjela", "hhh", 18);
-
+		Person person4 = new Person("Anjela", "Ahh", 18);
+		
 		List<Person> persons = new ArrayList<>();
 		persons.add(person);
 		persons.add(person1);
 		persons.add(person2);
 		persons.add(person3);
 		persons.add(person4);
-
-		persons.forEach(p -> System.out.println(p));
-
-//		Comparator<Person> lastNameComporataor = new Comparator<Person>() {
-//
-//			@Override
-//			public int compare(Person o1, Person o2) {
-//
-//				return o1.getLastName().compareTo(o2.getLastName());
-//			}
-//
-//		};
-
-		Collections.sort(persons, (o1, o2) -> o1.getLastName().compareTo(o2.getLastName()));
-		System.out.println();
-		persons.forEach(p -> System.out.println(p));
-		System.out.println();
-
-		persons.forEach(p -> {
-			if (!p.getFirstName().contains("o")) {
-				System.out.println(p);
-			}
-		});
-		System.out.println();
-
-		persons.forEach(p -> {
-			if (p.getAge() <= 21) {
-				System.out.println(p);
-			}
-		});
+		
+       persons.forEach(p -> System.out.println(p));
+       System.out.println();
+       
+       Collections.sort(persons,(p1,p2) -> p1.getLastName().compareTo(p2.getLastName()));
+       persons.forEach(p -> System.out.println(p));
+       System.out.println();
+       
+       persons.forEach(p ->{
+    	   if(!p.getFirstName().contains("o")) {
+    		   System.out.println(p);
+    	   }
+       });
+       
+       System.out.println();
+       
+       List<Person> under21 = persons.stream().filter(p -> p.getAge() <= 21).collect(Collectors.toList());
+       under21.forEach(p -> System.out.println(p));
+		
+		
 
 	}
 
