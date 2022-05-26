491f4d215e4d0b1c5894cf74ee293c592d55cde0
AlexBatura1988 <alex.batura88@gmail.com>
2022-05-26 14:33:55 +0300

    TCP

--- networking/src/TCP/TcpClient.java
+++ networking/src/TCP/TcpClient.java
@@ -1,5 +1,20 @@
 package TCP;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
 public class TcpClient {
+	private final  int PORT = 8080;
+	private final static String HOSTNAME = "localhost";
+	private Socket clientSocket;
+	private PrintWriter out;
+	private BufferedReader in;
+	
+	public TcpClient() throws UnknownHostException, IOException {
+		clientSocket = new Socket(HOSTNAME,PORT);
+	}
 
 }
--- networking/src/TCP/TcpServer.java
+++ networking/src/TCP/TcpServer.java
@@ -1,5 +1,47 @@
 package TCP;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class TcpServer {
+	public static final int PORT = 4413;
+    
+    private ServerSocket serverSocket;
+  
+    public TcpServer() throws IOException {
+    	this.serverSocket = new ServerSocket(PORT);
+        System.out.println("New client connected from " + serverSocket.getInetAddress().getHostAddress());
+       
+    }
+  
+    public void start() throws IOException {
+    	Socket socket = serverSocket.accept();
+        InputStream in = null;
+        try {
+            in = socket.getInputStream();
+            BufferedReader br = new BufferedReader(new InputStreamReader(in));
+            String request;
+            request = br.readLine();
+            System.out.println("Message received:" + request);
+        } catch (IOException ex) {
+            System.out.println("Unable to get streams from client");
+        } finally {
+            try {
+                in.close();
+                socket.close();
+                System.out.println("Socket closed");
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            }
+        }
+    }
+    public static void main(String[] args) throws IOException {
+		TcpServer server = new TcpServer();
+		server.start();
+	}
 
 }
